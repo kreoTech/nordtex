eff2af8d0dba8e2517296917045fe3ad506e8041
Алексей <kreo.tech@lunpully.com>
2018-12-16 16:38:33 +0500

    Мобильное меню поправлено

--- src/js/components/_sandwich.js
+++ src/js/components/_sandwich.js
@@ -1,6 +1,8 @@
 $('.toggle-mnu').on('click', function() {
   const sandwich = $('.sandwich');
+  const $body = $('body');
   const sideMenu = $('.header__wrap');
   sandwich.toggleClass('is-active');
+  $body.toggleClass('menuOpen');
   sideMenu.toggleClass('is-active');
 });
